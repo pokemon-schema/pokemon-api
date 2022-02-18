7733ce19d6dacea41095cb19c39991a9adba1e9a
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2022-02-17 23:12:15 -0300

    selectPokemonById

--- src/main/java/com/api/pokemonapi/controller/PokemonController.java
+++ src/main/java/com/api/pokemonapi/controller/PokemonController.java
@@ -1,16 +1,26 @@
 package com.api.pokemonapi.controller;
 
+import java.util.Optional;
+
+import com.api.pokemonapi.entity.Pokemon;
+import com.api.pokemonapi.repository.PokemonRepository;
 import com.api.pokemonapi.service.PokemonService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
+@CrossOrigin
 public class PokemonController {
 
+    // service
     @Autowired
-    PokemonService service;
+    private PokemonService service;
 
     @RequestMapping("/")
     public String helloWorld() {
@@ -22,4 +32,17 @@ public class PokemonController {
         return service.getPokemonJson();
     }
 
+    @RequestMapping("/pokemons/id={id}")
+    public String selectById(@PathVariable(value = "id") Long id) {
+        return service.findById(id);
+    }
+
+    // // repository
+    // @Autowired
+    // private PokemonRepository repository;
+
+    // @GetMapping("/pokemons/id={id}")
+    // public Optional<Pokemon> selectById(@PathVariable(value = "id") Long id) {
+    // return repository.findById(id);
+    // }
 }
--- src/main/java/com/api/pokemonapi/service/PokemonService.java
+++ src/main/java/com/api/pokemonapi/service/PokemonService.java
@@ -1,5 +1,8 @@
 package com.api.pokemonapi.service;
 
+import java.util.Optional;
+
+import com.api.pokemonapi.entity.Pokemon;
 import com.api.pokemonapi.repository.PokemonRepository;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -9,18 +12,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PokemonService {
-    // repository
-    private PokemonRepository pokemonRepository;
 
     // poke-api
     private final RestTemplate restTemplate;
+    String baseUrl = "https://pokeapi.co/api/v2/";
 
     public PokemonService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
     public String getPokemonJson() {
-        String url = "https://pokeapi.co/api/v2/pokemon/1";
+        String url = baseUrl + "pokemon/";
+        return this.restTemplate.getForObject(url, String.class);
+    }
+
+    public String findById(Long id) {
+        String idStr = String.valueOf(id);
+        String url = baseUrl + "pokemon/" + idStr; // http://localhost:8080/pokemons/id=1
         return this.restTemplate.getForObject(url, String.class);
     }
 
