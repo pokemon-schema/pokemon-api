08c1e44a292deaba1a0fb9c3fc3d5df04b80ded9
andre <andre.myszko@gmail.com>
2022-03-29 16:38:56 -0300

    update. add swagger docs api

--- src/main/java/com/api/pokemonapi/PokemonApiApplication.java
+++ src/main/java/com/api/pokemonapi/PokemonApiApplication.java
@@ -1,13 +1,28 @@
 package com.api.pokemonapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class PokemonApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokemonApiApplication.class, args);
+	}
+
+	@Bean // adding swagger to api documentation -> usage: .../swagger-ui.html
+	public OpenAPI customOpenAPI(@Value("${spring.application.name}") String description) {
+		return new OpenAPI().info(new Info()
+				.title(description)
+				.version("1.0")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
--- src/main/java/com/api/pokemonapi/controller/PokemonController.java
+++ src/main/java/com/api/pokemonapi/controller/PokemonController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class PokemonController {
 
     // service
